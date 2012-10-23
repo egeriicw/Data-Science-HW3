b080f945571a65c9c390c66940b3eb359e2c4015
Michael Discenza <michael.discenza@gmail.com>
2012-10-23 09:03:17 -0400

    Getting there on the graph

--- R/hw3_problem4.R
+++ R/hw3_problem4.R
@@ -1,24 +1,38 @@
 #Code for the get glue problem
+install.packages("stringr")
+install.packages("parallel")
 
+"knitr" and "foreach"
 #code from the HW
 #install.packages("RJSONIO")
 library("RJSONIO")
+library(plyr)
 file.path <- "http://getglue-data.s3.amazonaws.com/getglue_sample.tar.gz"
-con <- gzcon(url(file.path))
+con <- gzcon(url(file.path)) #opens up a connection that allows you to decompress and read from file
 # first trying it with 10 lines:
 n.lines.to.read <- 10
-temp <- readLines(con, n.lines.to.read)[2:length(temp)] # SYKE! JUST 9 LINES!
+lines <- readLines(con, n.lines.to.read) # SYKE! JUST 9 LINES!
 #you need to take out the name of the json file because r just treats it as a string and it messes up rjsonio
-gmgfromJSON(temp)
-n.lines.to.read <- 1000
-tmp <- readLines(con, n.lines.to.read)
-head(tmp)
-close(con)
+theRead <- lapply(lines[-1],fromJSON) #iterates through 1 element at a time through the list
+str(theRead) # we have a list of lists
+theData <-ldply(theRead, as.data.frame) # this turns the results of the operations on list as a dataframe
+theData
 
 
-res  <- fromJSON(temp[2:length(temp)]) # tokenize
-dat <- unlist(res$results) # convert the dates to a vector
 
-head(temp[2:])
 
-temp[3]
+
+temp2 <-fromJSON(temp)
+dat <- unlist(temp2)
+dat
+as.data.frame(re[[1]])
+resList1<-lapply(res1$results, as.data.frame)
+ldply(resList1)
+test<- unlist(temp)
+
+result <- 
+for(i in 0:length(temp))
+{
+    result[[i+1]]<-ldply(temp[[i+1]], as.data.frame)
+}
+sapply
---
+++ R/hw3_visualization.R
@@ -0,0 +1,40 @@
+crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState2005.tsv", header=TRUE, sep="\t")
+#Flowing Data Tutorial Distruction
+radius <- sqrt( oc$size/ pi)
+symbols(oc$M_w_earnings, oc$W_w_earnings, circles=radius, inches=0.35, fg="white", bg="red", xlab="Men's Weekly Earnings in $", ylab="Women's Weekly Earnings", main="Income Disparity by Occupation and Gender")
+text(oc$M_w_earnings, oc$W_w_earnings, oc$Occupational.Group, cex=0.5)
+
+
+#Can't use xy, ehhhh?
+radius <- sqrt( oc$size/ pi)
+xysymbols(oc$M_w_earnings, oc$W_w_earnings, circles=radius, inches=0.35, fg="white", bg="red", xlab="Men's Weekly Earnings in $", ylab="Women's Weekly Earnings", main="Income Disparity by Occupation and Gender")
+text(oc$M_w_earnings, oc$W_w_earnings, oc$Occupational.Group, cex=0.5)
+
+
+
+
+
+library(ggplot2)
+library(directlabels)
+#my data set:
+oc <- read.csv("~/Dropbox/Fall_2012/Intro_to_Data_Science/Assignments/HW3/Data Science HW3/Vizualization/oc.csv")
+oc$percent_women <- oc$W_employment/(oc$M_employment+oc$W_employment)
+oc$size <- oc$W_employment+oc$M_employment
+p <- ggplot(oc, aes(M_w_earnings, W_w_earnings, label = as.character(Occupational.Group)))
+p + geom_point(aes(size = size, colour=percent_women)) + scale_size_continuous(range=c(1,30)) + #scale_area()+
+    #geom_point(aes(colour = oc$percent_women)) + 
+    coord_equal() +
+    scale_colour_gradient(high = "red", low="blue")+
+    ylim(700, 1700) +
+    xlim(700, 1700) +
+    geom_abline(slope=1) +
+    labs(title = "Income Disparity by Occupation and Gender") +
+    ylab("Women's Weekly Earnings in $") +
+    xlab("Men's Weekly Earnings in $") +
+    geom_text(aes(label=Occupational.Group), size=5, angle = 0)
+
+
+library(xtable)
+oc_2 <- oc[1:6]
+xtable(oc_2)
+
